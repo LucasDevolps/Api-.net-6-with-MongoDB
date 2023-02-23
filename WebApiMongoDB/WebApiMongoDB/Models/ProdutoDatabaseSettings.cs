2dbb531b667e68c02a13b51408e2c5f6a5030850
Lucas Ribeiro <lucasribeirodesouza2000@gmail.com>
2023-02-22 22:55:11 -0300

    Subindo Api

---
+++ WebApiMongoDB/WebApiMongoDB/Models/ProdutoDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiMongoDB.Models
+{
+    public class ProdutoDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null;
+
+        public string DatabaseName { get; set; } = null;
+
+        public string ProdutoCollectionName{ get; set; } = null;
+    }
+}
